ac373815255ca4d246f85fac5b04758773ae6119
captinturtle1 <49005171+captinturtle1@users.noreply.github.com>
2024-01-21 21:54:16 -0700

    added more NPCS

--- index.js
+++ index.js
@@ -4,22 +4,28 @@ import { Client, Events, GatewayIntentBits } from "discord.js";
 const secWarning = 300;
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+client.login(process.env.DISCORD_TOKEN);
 
 client.once(Events.ClientReady, async client => {
     console.log(`${client.user.tag} logged in!`);
-    pogLoop();
+    alertLoop(process.env.MECHANIC_ROLE_ID, "mechanic", process.env.CHANNEL_ID);
+    alertLoop(process.env.PRAPOR_ROLE_ID, "prapor", process.env.CHANNEL_ID);
+    alertLoop(process.env.PEACEKEEPER_ROLE_ID, "peacekeeper", process.env.CHANNEL_ID);
 });
 
-async function pogLoop() {
+async function alertLoop(role_id, name, channel_id) {
+    console.log(`starting ${name} loop!`);
     while (true) {
         try {
             let refreshTime = await getRefreshTime();
+            refreshTime = refreshTime.mechanic;
+            console.log(`${name} time: `, refreshTime);
             await new Promise(resolve => {
                 if (refreshTime > 0) {
                     setTimeout(async () => {
-                        client.channels.cache.get(process.env.CHANNEL_ID).send(`<@&${process.env.ROLE_ID}> MECHANIC REFRESH IN ${secWarning / 60} MINUTES!!!!!!!!!`);
+                        client.channels.cache.get(channel_id).send(`<@&${role_id}> ${name.toUpperCase()} REFRESH IN ${secWarning / 60} MINUTES!!!!!!!!!`);
                         await new Promise(res => setTimeout(res, (secWarning - 60) * 1000))
-                        client.channels.cache.get(process.env.CHANNEL_ID).send(`<@&${process.env.ROLE_ID}> MECHANIC REFRESH IN 1 MINUTE!!!!!!!!!`);
+                        client.channels.cache.get(channel_id).send(`<@&${role_id}> ${name.toUpperCase()} REFRESH IN 1 MINUTE!!!!!!!!!`);
                         await new Promise(res => setTimeout(res, 70 * 1000));
                         resolve();
                     }, (refreshTime - secWarning) * 1000);
@@ -34,8 +40,6 @@ async function pogLoop() {
         }
     }
 }
-
-client.login(process.env.DISCORD_TOKEN);
 
 async function getRefreshTime () {
     return new Promise((resolve, reject) => {
@@ -60,15 +64,14 @@ async function getRefreshTime () {
                 fetch('https://api.tarkov.dev/graphql', options)
                 .then(response => response.json())
                 .then(data => {
-                    let mechanicReset;
-                    let traders = data.data.traders
-
+                    let traders = data.data.traders;
+                    let resetObj = {};
+                    
                     for (let i = 0; i < traders.length; i++) {
-                        if (traders[i].name == "Mechanic") mechanicReset = new Date(traders[i].resetTime);
+                        resetObj[traders[i].name.toLowerCase()] = (new Date(traders[i].resetTime).getTime() - Date.now()) / 1000;
                     }
 
-                    let differenceInSec = (mechanicReset.getTime() - Date.now()) / 1000;
-                    resolve(differenceInSec);
+                    resolve(resetObj);
                 });
     } catch(err) {
         reject(err);
